2ab3f8fce85a867d4bf2d13f7ed8d4c33808582b
Hudson Mendes <hudsonmendes@gmail.com>
2023-11-03 14:08:25 +0000

    qa: answering from pre-trained knowledge

--- src/hlm12rag/modelling.py
+++ src/hlm12rag/modelling.py
@@ -1,7 +1,30 @@
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+
+
 class RagQA:
     """
     Coordinates Document Retrieval, Question Answering and Response Generation.
     """
 
+    def __init__(self, llm: HuggingFacePipeline) -> None:
+        self.llm = llm
+        self.prompt = ChatPromptTemplate.from_template(
+            "\n".join(
+                [
+                    "You are an assistant for question-answering tasks.",
+                    "If you don't know the answer, just say that you don't know.",
+                    "Provide the shortest possible answer.",
+                    "",
+                    "Question: ```{question}```",
+                    "Answer: ",
+                ]
+            )
+        )
+        self.qa = LLMChain(llm=llm, prompt=self.prompt)
+
     def ask(self, question: str) -> str:
-        return "king"
+        x = dict(question=question)
+        y = self.qa(x)
+        return y["text"]
--- tests/test_modelling.py
+++ tests/test_modelling.py
@@ -1,12 +1,37 @@
 import unittest
 
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from hlm12rag.modelling import RagQA
 
 
 class TestRagQA(unittest.TestCase):
     def setUp(self) -> None:
-        self.qa = RagQA()
+        self.pairs_generic = {
+            "Berlin or London": "London",
+            "In which continent is Brazil located?": "South America",
+            "What's the largest ocean in the world?": "Pacific Ocean",
+        }
+        self.pairs_specific = {
+            "What's arguslweruna role?": "king",
+            "What is the bog near ag45i4nt like?": "dry",
+            "What is 4831asx capable of?": "shooting lasers",
+        }
 
-    def test_ask_produces_response(self):
-        answer = self.qa.ask("What's arguslweruna role?")
-        self.assertEqual("king", answer)
+        llm = HuggingFacePipeline.from_model_id(
+            task="text2text-generation",
+            model_id="google/flan-t5-small",
+            model_kwargs=dict(temperature=10e-16, max_length=64, do_sample=True),
+        )
+        self.qa = RagQA(llm=llm)
+
+    def test_ask_answers_from_knowledge(self):
+        for question, expected in self.pairs_generic.items():
+            actual = self.qa.ask(question)
+            print((question, expected, actual))
+            self.assertEqual(expected, actual)
+
+    def test_ask_answers_from_docs(self):
+        for question, expected in self.pairs_specific.items():
+            actual = self.qa.ask(question)
+            print((question, expected, actual))
+            self.assertEqual(expected, actual)
